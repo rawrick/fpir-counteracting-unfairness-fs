4d1491e0807bcba384ca09831a00d2d4c29eaa67
Patrick Balla <63912835+rawrick@users.noreply.github.com>
2023-01-25 08:27:43 +0100

    Remoce Prolific Button

--- src/pages/thank-you.tsx
+++ src/pages/thank-you.tsx
@@ -15,15 +15,6 @@ const ThankYou: NextPage = () => {
         <div className="space-y-2">
           <p>You've done it! Thank you for your participation.</p>
         </div>
-        <div className="space-y-6">
-          <p>
-            To record that you have completed the task, please click on the
-            button that takes you back to the Prolific platform.
-          </p>
-          <Link href="https://app.prolific.co/submissions/complete?cc=CV7H9VDU">
-            <Button>Back to Prolific</Button>
-          </Link>
-        </div>
       </PageContainer>
     </>
   );

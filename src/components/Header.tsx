6ada5eaa84228c183b4c07f0f1a1dea895bb92ea
Patrick Balla <63912835+rawrick@users.noreply.github.com>
2023-03-21 14:58:16 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <header
       className={`flex justify-between p-2 bg-slate-200 sticky top-0 z-10 text-slate-700`}>
-      <h2 className="text-lg font-bold">Study Header</h2>
+      <h2 className="text-lg font-bold">Counteracting unfairness in featured snippets through visualisation of distribution</h2>
     </header>
   );
 };
